a674cdb46225641f36987127ca2e675189e9a6e7
Евгений Ширяев <xvolkx48@gmail.com>
2018-01-12 23:37:13 +0300

    New Variant
    A new version of the project. Completely empty, unchanged working files

--- ConsoleApplication1/sort_lapf.hpp
+++ ConsoleApplication1/sort_lapf.hpp
@@ -3,21 +3,5 @@
 #include<list>
 #include<memory>
 using namespace std;
-//unsigned short id_n1=0 ;
-//unsigned short id_n2=0 ;
 
-
-typedef void(*TDataReady1)  (unsigned char *buff, int leght);
-class  sort_lapf
-{
-public:
-	 sort_lapf(TDataReady1 FDdata);
-	~ sort_lapf();
-	void SortLapf(unsigned char *buff, int size_buff);
-private:
-	TDataReady1 Fdata;
-	list<shared_ptr<my>>id_list; 
-	list<shared_ptr<my>>::iterator Iter_sort;
-	unsigned short id_n;
-};
 
