68f1b8f672b687f78f134d7a45d5617e200074ae
Евгений Ширяев <xvolkx48@gmail.com>
2018-01-23 09:02:55 +0300

    Add file_header string for out file

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,13 +9,18 @@ using namespace std;
 
 const string output_file = "out.ips";
 const string input_file = "1.ips";
+const string file_header = "IP_STREAM";
 
 void writeDataToFile(unsigned short channel, unsigned char *bufer, int size)
 {
 	ofstream ofs(output_file, ios::app|ios::binary);
 	if (!ofs)
 		throw runtime_error("Cannot open ouput file");
+
+	//
+	//запись канала и размера пакета
 	ofs << channel << size;
+	//запись данных
 	for (int i = 0; i != size; i++)
 	{
 		ofs << bufer[i];
@@ -35,12 +40,16 @@ int main()
 	if (!ips.is_valid())
 		throw runtime_error("Something wrong with ip stream");
 
+	ofstream ofs(output_file, ios::out | ios::binary);
+	ofs.write(file_header.c_str(), file_header.length());
+
 	while (!ips.end()) {
 		Packet packet{ ips.get() };
 		if (packet.isLapfPacket()) {
 			lapfVault->process(packet);
 		}
 	}
+	cout << "Press any key:";
 	getch();
 	return 0;
 }
